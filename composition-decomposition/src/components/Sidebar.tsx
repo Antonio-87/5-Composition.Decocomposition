40414195ebac67dd4923f4767f01fcdce7c6881d
Antonio-87 <davydenko.87@list.ru>
2023-10-25 12:26:47 +0300

    description Sidebar

--- composition-decomposition/src/components/Sidebar.tsx
+++ composition-decomposition/src/components/Sidebar.tsx
@@ -6,6 +6,7 @@ const Sidebar = ({
   className,
   children,
 }: PropsWithChildren<{ className: string }>) => {
+  /**Реализует боковую панель с любыми дочерними компонентами */
   return <aside className={className}>{children}</aside>;
 };
 
